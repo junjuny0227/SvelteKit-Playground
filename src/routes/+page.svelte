934d8a4157727b9da2c85b2904d625430f29c262
junjuny0227 <junjuny0227@gmail.com>
2024-10-12 21:40:19 +0900

    First time using a svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,11 @@
-<h1>Welcome to SvelteKit Playground!</h1>
+<script>
+	let name = '준연';
+</script>
+
+<h1>Hello {name}. Welcome to the <span>SvelteKit</span> Playground!</h1>
+
+<style>
+	h1 span {
+		color: #ff3e00;
+	}
+</style>
