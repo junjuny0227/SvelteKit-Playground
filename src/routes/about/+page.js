d4bb249ad9d398b9892f2f98990c0cc673fb0a77
junjuny0227 <junjuny0227@gmail.com>
2024-10-12 22:31:05 +0900

    fix: hide API key

--- src/routes/about/+page.js
+++ src/routes/about/+page.js
@@ -1,7 +1,8 @@
 export async function load({ fetch }) {
-	let res = await fetch(
-		'https://api.openweathermap.org/data/2.5/weather?lat=35.1585&lon=126.9254&appid=80a687f77790f783a6e9de99135496e0'
+	const apiKey = import.meta.env.VITE_OPENWEATHER_API_KEY;
+	const res = await fetch(
+		`https://api.openweathermap.org/data/2.5/weather?lat=35.1585&lon=126.9254&appid=${apiKey}`
 	);
-	let result = await res.json();
+	const result = await res.json();
 	return result;
 }
